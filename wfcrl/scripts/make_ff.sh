cff59a54836226a4e936732c6aca35e2d2285fb2
cibeah <cibeah.cb@gmail.com>
2024-06-13 09:56:54 +0200

    fix install script

--- wfcrl/scripts/make_ff.sh
+++ wfcrl/scripts/make_ff.sh
@@ -10,8 +10,10 @@ MPIF90_PATH=$(which mpif90)
 cmake .. -DCMAKE_Fortran_COMPILER=${MPIF90_PATH}
 make
 make install
+cd ../../
+cp ../servo_dll/DISCON.dll ../servo_dll/DISCON_WT1.dll
 
-cd ../../SC_DLL
+cd SC_DLL
 rm -f -r build && mkdir build && cd build
 MPIF90_PATH=$(which mpif90)
 cmake .. -DCMAKE_Fortran_COMPILER=${MPIF90_PATH}
